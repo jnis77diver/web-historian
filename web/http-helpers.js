aa3e51ee1f9bb8c7ef8be0aebe3cb7b0a62724e6
Jonah Nisenson <jsnisenson@gmail.com>
2015-02-26 15:46:56 -0500

    storing html to sqlite database in project. From index.html and loading.html, will load a webpage if in DB or store it if not.

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -2,6 +2,66 @@ var path = require('path');
 var fs = require('fs');
 var querystring = require('querystring');
 var archive = require('../helpers/archive-helpers');
+var http = require('http-request');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  storage: 'database.sqlite'
+});
+//CREATE TABLE IN DATABASE FOR HTML DATA
+var HTML = sequelize.define('html', {
+  url: {
+    type: Sequelize.STRING
+  },
+  html: {
+    type: Sequelize.STRING
+  }
+});
+//CREATE TABLE IN DATABASE FOR CSS DATA
+var CSS = sequelize.define('css', {
+  url: {
+    type: Sequelize.STRING
+  },
+  css: {
+    type: Sequelize.STRING
+  }
+});
+//CREATE TABLE IN DATABASE FOR JS DATA
+var JS = sequelize.define('js', {
+  url: {
+    type: Sequelize.STRING
+  },
+  js: {
+    type: Sequelize.STRING
+  }
+});
+//CODE BELOW FORCES CREATION OF DATABASE
+//HTML.sync({force: true}).then(function() {
+//  return HTML.create({
+//    url: 'http://www.testdata.com',
+//    html: "<html></html>"
+//  })
+//});
+//CSS.sync({force: true}).then(function() {
+//  return CSS.create({
+//    url: 'http://www.testdata.com/styles.css',
+//    css: ".body { color: red }"
+//  })
+//});
+//JS.sync({force: true}).then(function() {
+//  return JS.create({
+//    url: 'http://www.testdata.com/script.js',
+//    js: "var data = true;"
+//  })
+//});
 
 exports.headers = headers = {
   "access-control-allow-origin": "*",
@@ -18,6 +78,12 @@ exports.serveAssets = function(res, asset, callback) {
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
 };
 
+/**
+ * This provides the initial html to the client.
+ * @param req
+ * @param res
+ * @param {String} indexString
+ */
 exports.indexResponse = function(req, res, indexString){
   var filePath = "./web/public" + req.url;
   console.log(filePath);
@@ -67,16 +133,43 @@ exports.sendResponse = function(res, data, statusCode){
 
 
 exports.collectData = function(req,res, callback){
-  console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-  //console.log("this is the request body", req);
-  var newData = ''
+  var newData = '';
   req.on('data', function(chunk){
     newData += chunk;
   });
   req.on('end', function(){
-    console.log("this is the new data", querystring.parse(newData).url);
-    var data = req;
-    callback(req, res, "loading.html");
+    var fullURL = querystring.parse(newData).url;
+    var check = /http:\/\/[\w\W]+/;
+    if( !check.exec(fullURL) ) {
+      fullURL = "http://" + fullURL;
+    }
+    //query database for URL, if exists return index.html of url
+    HTML.find({where: {url: fullURL}}).then(function(html) {
+      if( html ) {
+        console.log('return html',html.html);
+        exports.sendResponse(res, html.html, 201);
+      }
+      //else, archive URL and return loading page
+      else{
+        http.get(fullURL, function (err, res) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          var site = res.buffer.toString();
+          HTML.sync().then(function() {
+            return HTML.create({
+              url: fullURL,
+              html: site
+            })
+          });
+          console.log("site ", site);
+        });
+        callback(req, res, "loading.html");
+      }
+    });
+
+
   });
 
 };
